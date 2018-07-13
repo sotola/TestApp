d940fe096e2d2494612b92fdb89e289d8583f5f1
Marek Sotola <sotola@gmail.com>
2018-07-13 07:19:52 -0400

    Add basic file upload support.

--- src/main/java/com/marek/testapp/StorageService.java
+++ src/main/java/com/marek/testapp/StorageService.java
@@ -1,4 +1,58 @@
 package com.marek.testapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+@Service
 public class StorageService {
+    HashMap<Path, FileSystemResource> fileStore = new HashMap<Path, FileSystemResource>();
+    Logger logger = LoggerFactory.getLogger(StorageService.class);
+
+    @Autowired
+    public StorageService() {}
+
+    void store(MultipartFile file) {
+        try {
+            File uploadedFile = Files.createTempFile("", "").toFile();
+            file.transferTo(uploadedFile);
+            fileStore.put(Paths.get(file.getOriginalFilename()), new FileSystemResource(uploadedFile));
+        } catch (IOException e) {
+            logger.warn(e.getLocalizedMessage());
+        }
+    }
+
+    Stream<Path> loadAll() {
+        return fileStore.keySet().stream();
+    }
+
+    Path load(String filename) {
+        Path path = Paths.get(filename);
+
+        if (fileStore.containsKey(path)) {
+            return path;
+        }
+
+        return null;
+    }
+
+    Resource loadAsResource(String filename) {
+        return fileStore.get(Paths.get(filename));
+    }
+
+    void deleteAll() {
+        fileStore.clear();
+    }
 }
--- src/main/java/com/marek/testapp/WebController.java
+++ src/main/java/com/marek/testapp/WebController.java
@@ -1,19 +1,34 @@
 package com.marek.testapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 
 @Controller
 public class WebController {
+    private final StorageService storageService;
+
+
+    @Autowired
+    public WebController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
     @GetMapping("/")
     public String personFormForm(Model model) {
         model.addAttribute("personForm", new PersonForm());
@@ -27,11 +42,31 @@ public class WebController {
         countries.sort(Comparator.naturalOrder());
         model.addAttribute("countries", countries);
 
-        return "form";
+       return "form";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
     @PostMapping("/")
-    public String personFormSubmit(@ModelAttribute PersonForm personForm) {
+    public String personFormSubmit(@RequestParam("file") MultipartFile file,
+                                   @ModelAttribute PersonForm personForm,
+                                   Model model) {
+        if (file != null) {
+            storageService.store(file);
+        }
+
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(WebController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+
         return "results";
     }
 }
